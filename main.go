b6b264790a8e8807a5b3b901ba1cf6b934d961a7
Malte Klena <Malte.Klena@brainwaregroup.com>
2019-02-13 14:36:31 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	pipe1()
+	fmt.Println("Done!")
+}
+
+// Pipeline, 3 Stages
+func pipe1() string {
+	fmt.Println("Pipe1 - Start!")
+
+	c1 := make(chan string, 2)
+	c2 := make(chan string, 2)
+
+	var result string
+
+	go stage1(c1)
+	go stage2(c1, c2)
+	result = stage3(c2)
+
+	fmt.Println("Pipe1 - Done!")
+	//fmt.Println(result)
+	return result
+}
+
+func stage1(cout chan string) {
+	// Stage 1: Werte erzeugen
+	for i := 0; i < 10; i++ {
+		var s = strconv.Itoa(i) + "-A"
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("s1: " + s)
+		cout <- s
+	}
+	close(cout)
+}
+
+func stage2(cin chan string, cout chan string) {
+	for s := range cin {
+		var x string
+		x = s + "B"
+		fmt.Println("s2: " + x)
+		cout <- x
+	}
+	close(cout)
+}
+
+func stage3(cin chan string) string {
+	var b = strings.Builder{}
+	for s := range cin {
+		var x string
+		x = s + "C"
+		fmt.Println("s3: " + x)
+		b.WriteString(x)
+		b.WriteString("\n")
+	}
+	return b.String()
+}
---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPipe1(t *testing.T) {
+	result := pipe1()
+
+	if result == "" {
+		t.Error("Result is missing")
+	}
+	if !strings.Contains(result, "ABC") {
+		t.Error("Result is not valid, no ABC")
+	}
+	//t.Error("Sucks 1")
+	//t.Error("Sucks 2")
+	t.Log(result)
+}
